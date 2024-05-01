5e7d3881d852cdaedaa6a36a9f11b2773d6e7391
Alex <sacre.alexandre@hotmail.com>
2024-05-01 14:19:23 +0200

    Slide Services, services et evaluations

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\EvaluationRepository;
+use App\Repository\ServiceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +11,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(ServiceRepository $servicesRepo,EvaluationRepository $evalRepo): Response
     {
-        return $this->render('home.html.twig', []);
+        return $this->render('home.html.twig', [
+            'services' => $servicesRepo->findAll(),
+            'evaluations' => $evalRepo->findBy([],null,3,null)
+        ]);
     }
 }
