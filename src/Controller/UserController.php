2c0f4496396c2cc232e759fec9e8a85302652984
Alex <sacre.alexandre@hotmail.com>
2024-05-26 14:03:46 +0200

    Début connexion User , switch Css des formulaire dans app.css

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,14 +5,33 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Exception\TooManyLoginAttemptsAuthenticationException;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    /**
+     * Permet à l'utilisateur de se connecter
+     *
+     * @param AuthenticationUtils $utils
+     * @return Response
+     */
+    #[Route('/login', name: 'account_login')]
+    public function index(AuthenticationUtils $utils): Response
     {
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+
+        $loginError = null;
+
+        if($error instanceof TooManyLoginAttemptsAuthenticationException){
+            $loginError = "Trop de tentatives de connexion. Veuillez attendre 15minutes";
+        }
+
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'error' => $error,
+            'username' => $username,
+            'loginError' => $loginError
         ]);
     }
 }
