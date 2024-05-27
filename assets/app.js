dc680b73799f908a9a5aa127760a5c5589a0b379
Alex <sacre.alexandre@hotmail.com>
2024-05-27 20:55:28 +0200

    Enlever le scroll to top de chaque page

--- assets/app.js
+++ assets/app.js
@@ -50,14 +50,3 @@ annulerDelete.forEach((annulerDelete,key)=>{
         alertDelete[key].classList.remove('active')
     })
 })
-
-// Permet de remonter jusqu'en haut de la page
-const scrollTop = document.querySelector('.scrollTop')
-
-scrollTop.addEventListener('click',()=>{
-    window.scrollTo({
-        top:0,
-        left:0,
-        behavior: "smooth"
-    })
-})
---
+++ assets/js/home.js
@@ -0,0 +1,10 @@
+// Permet de remonter jusqu'en haut de la page
+const scrollTop = document.querySelector('.scrollTop')
+
+scrollTop.addEventListener('click',()=>{
+    window.scrollTo({
+        top:0,
+        left:0,
+        behavior: "smooth"
+    })
+})
