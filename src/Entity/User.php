ee1098b11cde920a13f24ffb68229c36296870bb
Alex <sacre.alexandre@hotmail.com>
2024-05-27 10:43:08 +0200

    Assert User + form pour inscription

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,10 +8,13 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields:['email'],message: "Un autre utilisateur possède déjà cette adresse e-mail, merci de la modifier")]
 #[ORM\HasLifecycleCallbacks]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
@@ -21,6 +24,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 180)]
+    #[Assert\Email(message:"Veuillez renseigner une adresse email valide")]
+    #[Assert\NotBlank(message:"Ce champ ne peut pas être vide")]
     private ?string $email = null;
 
     /**
@@ -33,9 +38,15 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @var string The hashed password
      */
     #[ORM\Column]
+    #[Assert\NotBlank(message:"Ce champ ne peut pas être vide")]
     private ?string $password = null;
 
+    #[Assert\EqualTo(propertyPath:"password", message:"Vous n'avez pas correctement confirmé votre mot de passe")]
+    #[Assert\NotBlank(message:"Ce champ ne peut pas être vide")]
+    public ?string $passwordConfirm = null;
+
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:"Ce champ ne peut pas être vide")]
     private ?string $username = null;
 
     #[ORM\Column(length: 255)]
---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Form\ApplicationType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class RegistrationType extends ApplicationType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email',EmailType::class,$this->getConfiguration('','Adresse E-mail'))
+            ->add('username',TextType::class,$this->getConfiguration('','Nom / Prénom'))
+            ->add('password',PasswordType::class,$this->getConfiguration('','Mot de passe'))
+            ->add('passwordConfirm',PasswordType::class,$this->getConfiguration('','Confirmer le mot de passe'))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
