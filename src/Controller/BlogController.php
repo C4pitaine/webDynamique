22238e7154608fd206dfc1a34c7b6864cd8d1b20
Alex <sacre.alexandre@hotmail.com>
2024-06-08 14:18:08 +0200

    Recherche pour le blog

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,34 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\SearchType;
 use App\Service\PaginationService;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
 {
-     /**
-     * Permet d'afficher tous les articles côté utilisateur
-     *
-     * @param PaginationService $pagination
-     * @param integer $page
-     * @return Response
-     */
-    #[Route('/blog/{page<\d+>?1}', name: 'blog')]
-    public function index(PaginationService $pagination,int $page): Response
-    {   
-        $pagination->setEntityClass(Article::class)
-                    ->setLimit(6)
-                    ->setPage($page)
-                    ->setSearch("")
-                    ->setTemplatePath("/partials/_pagination.html.twig");
-
-        return $this->render('blog/index.html.twig', [
-           'pagination' => $pagination,
-        ]); 
-    }
-
     /**
      * Permet d'afficher un article côté utilisateur
      *
@@ -43,5 +24,41 @@ class BlogController extends AbstractController
         return $this->render('/blog/show.html.twig',[
             'article' => $article
         ]);
+    }
+
+    /**
+     * Permet d'afficher tous les articles côté utilisateur
+     *
+     * @param PaginationService $pagination
+     * @param integer $page
+     * @return Response
+     */
+    #[Route('/blog/{page<\d+>?1}/{recherche}', name: 'blog')]
+    public function index(PaginationService $pagination,Request $request,int $page,string $recherche=""): Response
+    {   
+        $pagination->setEntityClass(Article::class)
+                    ->setLimit(6)
+                    ->setPage($page)
+                    ->setSearch($recherche)
+                    ->setTemplatePath("/partials/_pagination.html.twig");
+        
+        $form = $this->createForm(SearchType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $recherche = $form->get('search')->getData();
+            if($recherche !== null){
+                $pagination->setSearch($recherche)
+                        ->setPage(1);
+            }else{
+                $pagination->setSearch("")
+                        ->setPage(1);
+            }
+        }
+
+        return $this->render('blog/index.html.twig', [
+           'pagination' => $pagination,
+           'formSearch' => $form->createView(),
+        ]); 
     }
 }
