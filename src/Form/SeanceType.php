ac1a34d45af4e771cc7876f4a276aad5e2bf828e
Alex <sacre.alexandre@hotmail.com>
2024-06-27 18:48:15 +0200

    Ajout d'un label pour les séances

--- src/Form/SeanceType.php
+++ src/Form/SeanceType.php
@@ -17,17 +17,18 @@ class SeanceType extends ApplicationType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name',TextType::class,$this->getConfiguration('','Nom de votre séance'))
-            ->add('date',DateType::class,[
+            ->add('name',TextType::class,$this->getConfiguration('Nom de votre séance','Nom de votre séance'))
+            ->add('date',DateType::class,$this->getConfiguration('Date de votre séance','Date de votre séance',[
                 'widget' => 'single_text',
-                'input'  => 'datetime_immutable'
-            ])
+            ]))
             ->add('exosMusculations', CollectionType::class, [
+                'label' => false,
                 'entry_type' => ExosMusculationType::class,
                 'allow_add' => true,
                 'allow_delete' => true
             ])
             ->add('exosCardios', CollectionType::class, [
+                'label' => false,
                 'entry_type' => ExosCardioType::class,
                 'allow_add' => true,
                 'allow_delete' => true
